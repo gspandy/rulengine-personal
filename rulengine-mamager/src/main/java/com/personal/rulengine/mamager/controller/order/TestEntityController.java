795721dbbe09e78eefa79f9e4f98e53693f0bd8b
liubao <18611478781@163.com>
2016-08-07 00:32:07 +0800

    晚上测试

--- rulengine-mamager/src/main/java/com/personal/rulengine/mamager/controller/order/TestEntityController.java
+++ rulengine-mamager/src/main/java/com/personal/rulengine/mamager/controller/order/TestEntityController.java
@@ -1,16 +1,24 @@
 package com.personal.rulengine.mamager.controller.order;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -47,11 +55,63 @@ public class TestEntityController extends CommonController {
     
 
     /**
+     * 测试文件下载类型请求,直接显示文件信息到浏览器中
      */
-    @RequestMapping(value = "/names")
+    @RequestMapping(value = "/download1")
     @ResponseBody
-    public Object testListPamas(@RequestParam("name")List<String> names ) {
-        logger.info("@RequestMapping:/test/welcome执行了...");
+    public void download(OutputStream out ) {
+        logger.info("@RequestMapping:/test/download执行了...");
+        
+        String pathname="D:\\temp\\VerifyCode.jpg";
+        InputStream in=null;
+        try {
+            in = new  FileInputStream(new File(pathname));
+            FileCopyUtils.copy(in, out);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    /**
+     * 直接下载文件
+     * http://localhost:8080/rulengine-mamager/server/test/download2
+     */
+    @RequestMapping(value = "/download2")
+    @ResponseBody
+    public void download2(OutputStream out,HttpServletResponse response ) {
+        logger.info("@RequestMapping:/test/download执行了...");
+        
+        response.setCharacterEncoding("utf-8");
+        response.setContentType("application/force-download");
+        response.setHeader("Content-Disposition", "attachement;filename=download.jpg");
+        
+        String pathname="D:\\temp\\VerifyCode.jpg";
+        InputStream in=null;
+        try {
+            in = new  FileInputStream(new File(pathname));
+            FileCopyUtils.copy(in, out);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+    }
+    
+    /**
+     * 测试传递List或者String[]格式的数据请求解析
+     * http://localhost:8080/rulengine-mamager/server/test/names1?name=1&name=liubao&name=wangli
+     */
+    @RequestMapping(value = "/names1")
+    @ResponseBody
+    public Object names1(@RequestParam("name")String[] names ) {
+        logger.info("@RequestMapping:/test/names1执行了...");
+        logger.info("参数信息为:{}",JSON.toJSONString(names));
+        return ResultInfoUtil.setErrorInfo(ErrorCodeConsField.ERROR_MSG_10003, 
+                getMessage(ErrorCodeConsField.ERROR_MSG_10003));
+    }
+    @RequestMapping(value = "/names2")
+    @ResponseBody
+    public Object names2(@RequestParam("name")List<String> names ) {
+        logger.info("@RequestMapping:/test/names2执行了...");
         logger.info("参数信息为:{}",JSON.toJSONString(names));
         return ResultInfoUtil.setErrorInfo(ErrorCodeConsField.ERROR_MSG_10003, 
                 getMessage(ErrorCodeConsField.ERROR_MSG_10003));
