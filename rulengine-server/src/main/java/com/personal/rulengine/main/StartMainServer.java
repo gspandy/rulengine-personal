579e5558aefe8955d129735c05c7bf727c5c8ab2
liubao <18611478781@163.com>
2016-09-09 16:12:06 +0800

    添加打包依赖信息

--- rulengine-server/src/main/java/com/personal/rulengine/main/StartMainServer.java
+++ rulengine-server/src/main/java/com/personal/rulengine/main/StartMainServer.java
@@ -1,5 +1,8 @@
 package com.personal.rulengine.main;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * 启动 dubbo 服务
  * 
@@ -11,9 +14,12 @@ package com.personal.rulengine.main;
  */
 public class StartMainServer {
     
+    private static final Logger logger = LoggerFactory.getLogger(StartMainServer.class);
+    
     public static void main(String[] args) {
         com.alibaba.dubbo.container.Main.main(args);
         System.out.println("StartMainServer.main执行完成.....");
+        logger.warn("StartMainServer.main执行完成.....");
     }
     
 }
